f137612fc82cf97265f27f2afd4978cf4ddbdaf1
vklimov <vklimov@livemaster.ru>
2018-06-30 00:38:34 +0300

    added sky

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+context.fillStyle = 'rgb(0, 100, 190)';
+context.fillRect(0, 0, canvas.width, canvas.height);
